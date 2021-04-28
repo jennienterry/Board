472450d7e1f3bf9bc2c6ad550dc2789d0f90468b
jennienterry <81270498+jennienterry@users.noreply.github.com>
2021-04-28 18:38:54 +0900

    Add files via upload

--- WebWorkspace/BoardVer1/src/main/java/com/koreait/board/Database.java
+++ WebWorkspace/BoardVer1/src/main/java/com/koreait/board/Database.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.ArrayList; 
 public class Database {
 	public static List<BoardVO> list = new ArrayList();
-	 			 // <>꺽새 있으면 BoardVO 객체 주소값만 사용가능, 지우면 개나소나 다 들어갈 수 있게됨
+	 			 // <>꺽새 있으면 BoardVO 객체 주소값만 사용가능, 지우면 개나소나 다 들어갈 수 있게 되니까 주의
 				 // 다 String으로 받기 때문에 넣거나 뺄 때 형변환 계속 해줘야해서 <>타입 정해주는 것
-				 // 주소값만 알면 다 명령내릴 수 있음
-	// stack에 올라가서 공용으로 바로 사용하기위해 static 사용 (주소값으로 접근이 바로 가능)
+	// 주소값만 알면 다 명령내릴 수 있음
+	// stack에 올라가서 공용으로 바로 사용하기위해 static 사용 (주소값으로 접근이 바로 가능) 그래서 Database.list로 접근가능
 	// 이것은 그냥 배열객체
+	// 모든class의 최상위객체가 object이듯, ArrayList의 최상위객체가 List
 }
