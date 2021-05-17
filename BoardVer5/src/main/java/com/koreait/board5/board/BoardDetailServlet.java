7174df1040573be43dc17f9375bd6b761a179482
jennienterry <81270498+jennienterry@users.noreply.github.com>
2021-05-17 17:27:45 +0900

    Add files via upload

--- BoardVer5/src/main/java/com/koreait/board5/board/BoardDetailServlet.java
+++ BoardVer5/src/main/java/com/koreait/board5/board/BoardDetailServlet.java
@@ -1,3 +1,40 @@
+<<<<<<< HEAD
+package com.koreait.board5.board;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.koreait.board5.DBUtils;
+import com.koreait.board5.MyUtils;
+import com.koreait.board5.cmt.CmtDAO;
+
+@WebServlet("/board/detail")
+public class BoardDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int iboard = MyUtils.getParamInt("iboard", request);
+		BoardVO vo = new BoardVO();
+		vo.setIboard(iboard);
+		vo = BoardDAO.selBoard(vo);
+		
+		request.setAttribute("vo", vo);
+		request.setAttribute("cmtList", CmtDAO.selCmtList(vo));
+		MyUtils.openJSP("board/detail", request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
+=======
 package com.koreait.board5.board;
 
 import java.io.IOException;
@@ -31,3 +68,4 @@ public class BoardDetailServlet extends HttpServlet {
 	}
 
 }
+>>>>>>> refs/remotes/origin/main
